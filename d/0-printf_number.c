0b7d98eaebf5aa7b77389f521638d85450d59e60
Tasneem Farag <tasneem.a.farag@gmail.com>
2016-02-25 17:38:33 -0800

    edit file

--- d/0-printf_number.c
+++ d/0-printf_number.c
@@ -1,15 +1,15 @@
+#include <stdio.h>
 #include "my_functions.h"
 /*function takes an integer*/
-void print_number(int n) {
+void printf_number(int n) {
   int ncopy, digi, power;
   int singledigi;
-  char chardigi;
   char digicopy;
   if (n < 0)
     { n = n * -1;
-      printf('-') }
+      printf("-"); }
   else if (n == 0)
-    { printf('0');
+    { printf("0");
       return ; }
   for (ncopy = n , digi = 0; ncopy > 0; digi ++)
     { ncopy = ncopy / 10 ; }
@@ -18,7 +18,6 @@ void print_number(int n) {
     {  power = power * 10; }
   for ( ;digicopy > 0; digicopy --)
     { singledigi = n/power;
-      chardigi = singledigi + '0';
-      printf(chardigi);
+      printf("%d", singledigi);
       n = n - (singledigi * power);
       power = power / 10;} }
