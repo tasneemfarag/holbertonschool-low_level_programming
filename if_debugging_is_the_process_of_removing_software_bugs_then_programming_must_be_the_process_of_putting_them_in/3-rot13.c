fa1f49bcbd70c03a63565fe3aac88b5ebc844397
Tasneem Farag <tasneem.a.farag@gmail.com>
2016-03-21 15:01:24 -0700

    add folder

---
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/3-rot13.c
@@ -0,0 +1,15 @@
+/*function that encode a string using rot13 */
+char *rot13(char *s){
+  int i;
+
+  for(i=0; s[i]!='\0'; i++){
+    if((s[i] >= 'A' && s[i] <='M') || (s[i] >= 'a' && s[i] <='m')){
+
+      s[i]+=13;     }
+
+    else if((s[i] >= 'N' && s[i] <='Z')|| (s[i] >= 'n' && s[i] <='z')){
+
+      s[i]-=13;     }
+  }
+  return s;
+}
