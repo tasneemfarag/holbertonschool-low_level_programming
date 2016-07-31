6c3aecfa2afa528e5a9ea80b0b68473987dfb85c
tasneemfarag <39@holbertonschool.com>
2016-07-31 21:24:53 +0000

    done with insert

---
+++ d/printing.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+  long l;
+  l = 80;
+
+  printf("Hello there\n");
+
+  printf("The size of an (int) is:\t");
+  printf("%lu\n", sizeof(l));
+
+  return 0;
+}
---
+++ n_trees/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+  int **dp;
+  
+  dp = malloc(sizeof(void *));
+  
+  *dp = malloc(sizeof(void *));
+  
+  **dp = 327;
+  
+  printf("dp\naddr:\t%p\n", (void *) &dp);
+  printf("value:\t%p\n", (void *) dp);
+  printf("-----------------------\n");
+  printf("ptr\naddr:\t%p\n", (void *) &(*dp));
+  printf("value:\t%p\n", (void *) *dp);
+  printf("-----------------------\n");
+  printf("n\naddr:\t%p\n", (void *) &(**dp));
+  printf("value:\t%d\n", **dp);
+  return 0;
+}
